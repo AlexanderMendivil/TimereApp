2be6fa79492ca8638d3589678397f98fa2a85922
Ivan Alexander Mendivil Araujo <alexanderIMA@outlook.es>
2021-12-07 17:39:38 -0700

    Se agrego capturar usuario en controller

--- App/controler/registerController.js
+++ App/controler/registerController.js
@@ -1,6 +1,11 @@
-import { auth } from '../model/firebase';
-
+import { auth, db } from '../model/firebase';
+import firebase from "../model/firebase"
 
 export const SignUp = (email, password) =>{
     return auth.createUserWithEmailAndPassword(email, password)
+}
+
+export const createUser = async (id, user) =>{
+    await db.collection("Users").doc(id).set(user)
+    
 }
