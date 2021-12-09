1b42ad386d18f2edd1f75661857f4b14c13511b2
Ivan Alexander Mendivil Araujo <alexanderIMA@outlook.es>
2021-12-08 18:50:49 -0700

    Se arregló error de userReducer

--- App/reducers/userReducer.js
+++ App/reducers/userReducer.js
@@ -1,16 +1,22 @@
 import { GET_USER } from "../actions/types";
+import { USE_USER } from "../actions/types";
 
 const initialState = {
     userId: ""
 }
 
-const userReducer = (state = initialState, GET_USER) => {
+const userReducer = (state = initialState, action) => {
 
     switch(action.type){
         case GET_USER:
             return {...state, 
-            userId: state.userId
+            userId: action.data
         }
+        case USE_USER:
+            return{
+                ...state, 
+                userId: action.key
+            }
         default:
             return state
     }
