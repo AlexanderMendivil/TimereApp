c9d9da3dcdfb19d5665fbda55d494497cfb6cce7
Ivan Alexander Mendivil Araujo <alexanderIMA@outlook.es>
2021-12-08 18:50:01 -0700

    Se agregaron actions de redux para user

--- App/actions/user_actions.js
+++ App/actions/user_actions.js
@@ -1,8 +1,16 @@
 import {GET_USER} from "./types"
+import { USE_USER } from "./types"
 
 export const getUser = (userId) =>(
     {
         type: GET_USER,
         data: userId
+    }
+)
+
+export const useUser = (key) =>(
+    {
+        type: USE_USER,
+        key: key
     }
 )
