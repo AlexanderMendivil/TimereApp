18ad1578b170a577217b373121256c8e0243ed65
Ivan Alexander Mendivil Araujo <alexanderIMA@outlook.es>
2021-12-08 18:47:31 -0700

    Se hizo wrapp de redux en app.js

--- App.js
+++ App.js
@@ -1,15 +1,20 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { StyleSheet } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { Provider } from 'react-redux';
 import { auth } from './App/model/firebase';
 import MainStack from './Navigation/MainStack';
 
+import configureStore from './App/store';
+const store = configureStore()
 export default function App() {
 
   return (
+    <Provider store={store}>
     <SafeAreaView style={styles.container}>
       <MainStack/>
     </SafeAreaView>
+    </Provider>
   );
 }
 
