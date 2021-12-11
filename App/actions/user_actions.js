8a93423701ff2c26f3f74f0984590ad5da308cd2
Ivan Alexander Mendivil Araujo <alexanderIMA@outlook.es>
2021-12-10 17:14:32 -0700

    Se eliminó action de usuario no usada

--- App/actions/user_actions.js
+++ App/actions/user_actions.js
@@ -1,16 +1,8 @@
 import {GET_USER} from "./types"
-import { USE_USER } from "./types"
 
 export const getUser = (userId) =>(
     {
         type: GET_USER,
         data: userId
-    }
-)
-
-export const useUser = (key) =>(
-    {
-        type: USE_USER,
-        key: key
     }
 )
