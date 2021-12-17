62c9376fd33315e2e39630d903b7fba2dd618192
Ivan Alexander Mendivil Araujo <alexanderIMA@outlook.es>
2021-12-17 16:10:34 -0700

    se agregó action para agarrar todo el usuario de firebase

--- App/actions/user_actions.js
+++ App/actions/user_actions.js
@@ -1,8 +1,16 @@
 import {GET_USER} from "./types"
+import { USE_USER } from "../actions/types";
 
 export const getUser = (userId) =>(
     {
         type: GET_USER,
         data: userId
+    }
+    )
+    
+    export const useUser = (user) => (
+    {
+        type: USE_USER,
+        data: user
     }
 )
