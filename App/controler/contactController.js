3a6bbf29fe57993f42748834307a2dd87ea52739
Ivan Alexander Mendivil Araujo <alexanderIMA@outlook.es>
2021-12-17 16:27:05 -0700

    Se agrego controller para contacto

--- App/controler/contactController.js
+++ App/controler/contactController.js
@@ -1,5 +1,13 @@
 import {db} from '../model/firebase';
 
-export const getContacts = async (id) =>{
-    return await db.collection("Contacts").where("userId", "==", id).get()
+export const getContacts = async (id) => {
+    return await db.collection("Contacts").where("id", "==", id).get()
+}
+
+export const createContacts = async (contact) => {
+    await db.collection("Contacts").add(contact)
+}
+
+export const uploadContact = async (id, contact) => {
+    return await db.collection("Contacts").where("id", "==", id).update(contact)
 }
