f3af7cf2f24b99e5f60bd0ee9d1fa88e52a4c39d
Alexander Mendivil <alexanderima@outlook.es>
2022-01-06 23:14:45 -0700

    Se agrego action para el id del documento del contacto

--- App/actions/contacts_actions.js
+++ App/actions/contacts_actions.js
@@ -1,10 +1,16 @@
-import {GET_CONTACTS, GET_CONTACTS_PHONE_NUMBERS, DELETE_CONTACTS_PHONE_NUMBERS} from "./types"
+import {GET_CONTACTS, GET_CONTACTS_PHONE_NUMBERS, DELETE_CONTACTS_PHONE_NUMBERS, GET_CONTACT_ID} from "./types"
 
 export const getContacts = (name, telefono) =>(
     {
         type: GET_CONTACTS,
         name: name,
         telefono: telefono
+    }
+)
+export const getContactsId = (id) =>(
+    {
+        type: GET_CONTACT_ID,
+        data: id
     }
 )
 
