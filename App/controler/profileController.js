bf42704e70d3fb791f76b2a17df32c6e8b3921bf
Alexander Mendivil <alexanderima@outlook.es>
2022-01-07 13:55:11 -0700

    Se eliminaron funciones no usadas

--- App/controler/profileController.js
+++ App/controler/profileController.js
@@ -1,4 +1,4 @@
-import { db, ref, state_changed, auth } from '../model/firebase';
+import { db, state_changed, auth } from '../model/firebase';
 
 export const getActualUser = async (id) =>{
     return await db.collection("Users").doc(id).get()
@@ -12,40 +12,3 @@ export const updateUser = async (id, user, email) =>{
     return await db.collection("Users").doc(id).update(user)
 }
 
-export const uploadImage = async ( image , uploading ) =>{
-    const blob = await new Promise((resolve, reject)=>{
-        const xhr = new XMLHttpRequest()
-        xhr.onload = function(){
-            resolve(xhr.response)
-        }
-
-        xhr.onerror = function(){
-            reject(new TypeError("Network request failed"))
-        }
-        xhr.responseType = "blob"
-        xhr.open("GET", image, true)
-        xhr.send(null)
-    })
-
-    ref.ref().child(new Date().toISOString())
-    const snapshot = ref.put(blob)
-
-    snapshot.on(state_changed,
-         ()=>{ uploading = true},
-          (err)=>{
-            uploading = false
-              console.log(err)
-              blob.close()
-              return
-            },
-           ()=>{snapshot.ref.getDownloadURL().then((url)=>{
-               uploading = false   
-               console.log(url)
-               blob.close()
-               return url
-
-                }
-            )
-        }
-    )
-}
